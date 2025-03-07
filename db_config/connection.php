281ef72d32d3dac5c2244d7608347883373c41ac
Ekundayo Mayowa T. <ekundayomayor@gmail.com>
2025-03-07 20:16:14 +0100

    db_con

---
+++ db_config/connection.php
@@ -0,0 +1,9 @@
+<?php
+// Database connection
+$db = mysqli_connect('localhost', 'root', '', 'cdlp_assessment');
+
+// Check connection
+if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
